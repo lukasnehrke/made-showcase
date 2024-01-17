d8c87e5896baed6e29004987f679e62718990007
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-01-17 19:48:02 +0100

    fix: use json google credentials

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -2,11 +2,7 @@ import { Firestore } from '@google-cloud/firestore';
 
 export const firestore = new Firestore({
   projectId: process.env.GOOGLE_PROJECT_ID,
-  credentials:
-    process.env.NODE_ENV === 'production'
-      ? {
-          client_email: process.env.GOOGLE_CLIENT_EMAIL,
-          private_key: process.env.GOOGLE_PRIVATE_KEY,
-        }
-      : undefined,
+  credentials: process.env.GOOGLE_CREDENTIALS
+    ? (JSON.parse(atob(process.env.GOOGLE_CREDENTIALS)) as never)
+    : undefined,
 });
