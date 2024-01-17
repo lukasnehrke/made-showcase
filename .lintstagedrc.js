6258d5928c3d4b351bbb42aad47d6df875c1fbf2
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-01-17 10:24:16 +0100

    chore: add lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require('node:path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+module.exports = {
+  '*': 'prettier --ignore-unknown --write',
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+};
