03b830e9d379cdcc2a45a488a5cb7684da544ff0
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-01-17 07:51:16 +0100

    refactor: add vercel eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const { resolve } = require('node:path');
+
+const project = resolve(__dirname, 'tsconfig.json');
+
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+    require.resolve('@vercel/style-guide/eslint/next'),
+  ],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  parserOptions: {
+    project,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  rules: {
+    'import/no-default-export': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
