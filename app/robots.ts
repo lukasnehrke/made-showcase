4901aff81f0c6621046fccbc8048ff8706e6fae3
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-01-28 17:24:38 +0100

    feat: update robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/api',
+      disallow: ['/api', '/contact'],
     },
   };
 }
