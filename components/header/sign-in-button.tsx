bab55f78a670b7f8ac147e6006942068c08d0934
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-01-28 17:30:42 +0100

    feat: make sign in button responsive

--- components/header/sign-in-button.tsx
+++ components/header/sign-in-button.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import * as React from 'react';
-import { Github } from 'lucide-react';
+import { Github, LogIn } from 'lucide-react';
 import { login } from '@/actions/auth';
 import { Button } from '@/components/button';
 
 export function SignInButton() {
   return (
     <form action={() => void login()}>
-      <Button type="submit" variant="ghost">
+      <Button className="hidden md:flex" type="submit" variant="ghost">
         <Github className="w-4 h-4 mr-2" />
         Sign in with GitHub
+      </Button>
+      <Button className="flex md:hidden" size="icon" type="submit" variant="ghost">
+        <LogIn className="w-4 h-4" />
+        <span className="sr-only">Sign in</span>
       </Button>
     </form>
   );
