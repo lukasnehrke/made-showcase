4ee7f3d6d6426340ab474715cbae2b2a38b3c989
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-01-28 15:50:03 +0100

    refactor: move api routes

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/updateProjects',
+      disallow: '/api',
     },
   };
 }
