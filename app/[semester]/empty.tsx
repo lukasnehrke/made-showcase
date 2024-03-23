e29d361271b4cf0892d85a4596ca6f5b1ef8b501
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-03-23 04:19:16 +0100

    feat: add priority to image

--- app/[semester]/empty.tsx
+++ app/[semester]/empty.tsx
@@ -5,7 +5,14 @@ import image from '@/assets/undraw_empty_street.svg';
 export function Empty() {
   return (
     <div className="flex flex-col items-center text-center">
-      <Image alt="empty street" className="max-w-[70%] md:max-w-screen-sm my-4" sizes="100vw" src={image as string} />
+      <Image
+        alt="empty street"
+        className="max-w-[70%] md:max-w-screen-sm my-4"
+        sizes="100vw"
+        src={image as string}
+        priority
+      />
+
       <p className="mt-3 mb-1 text-xl font-medium">This page is empty</p>
       <p className="text-muted-foreground text-sm">No projects could be found for your search query.</p>
     </div>
