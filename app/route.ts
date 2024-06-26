b7033b42d622aed65246b9fe4571ba7a1b02fa2e
Lukas Nehrke <lukasnehrke@protonmail.com>
2024-06-26 12:51:42 +0200

    feat: add cron jobs

--- app/route.ts
+++ app/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from 'next/server';
-import { semesters } from '@/lib/constants';
 
 export const runtime = 'edge';
 
 export function GET(request: Request) {
-  return NextResponse.redirect(new URL(`/${semesters.at(-1)!}`, request.url));
+  return NextResponse.redirect(new URL(`/ws23`, request.url));
 }
